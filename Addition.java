d2abef8e480811396111c74a40d2d5d0888f47b5
Nagbhushan244 <01fe23bca244@kletech.ac.in>
2025-04-09 14:47:15 +0530

    Save Java File with changes

--- Addition.java
+++ Addition.java
@@ -5,7 +5,7 @@ public class Addition {
         b = 5;
         result1 = a + b;
         result2 = a - b;
-        System.out.println("The Sum Of Two Numbers is :" + result);
-        System.out.println("The Sub Of Two Numbers is :" + result);
+        System.out.println("The Sum Of Two Numbers is :" + result1);
+        System.out.println("The Sub Of Two Numbers is :" + result2);
     }
 }
