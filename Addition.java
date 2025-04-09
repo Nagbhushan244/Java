bb02b53e877431d7a91c03e377b18426fcb93f66
Nagbhushan244 <01fe23bca244@kletech.ac.in>
2025-04-09 14:05:50 +0530

    Save Java File

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a, b, result;
+        a = 10;
+        b = 5;
+        result = a + b;
+        System.out.println("The Sum Of Two Numbers is :" + result);
+    }
+}
